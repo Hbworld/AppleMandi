b14db81f086918c72cb49720387475adf3c9d705
Hemant <contact@hemantbansal.com>
2023-03-15 14:50:48 +0530

    adding domain-layer test cases

--- app/src/main/java/com/applemandi/android/view/MainActivity.kt
+++ app/src/main/java/com/applemandi/android/view/MainActivity.kt
@@ -1,14 +1,9 @@
 package com.applemandi.android.view
 
 import android.os.Bundle
-import android.util.Log
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.coroutineScope
 import com.applemandi.android.R
-import com.applemandi.android.viewModel.SellViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
